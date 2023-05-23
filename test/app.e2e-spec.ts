6d8d4ed9f1d044a546a2e1b290014d2a438c4c34
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-05-23 17:12:42 +0200

    test: begin test structure

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -11,11 +14,53 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it.todo('should register a user and return a token');
+    });
+    describe('Signin', () => {
+      it.todo('should authenticate a user and return a token');
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {
+      it.todo('should return currently logged in user');
+    });
+
+    describe('Edit user', () => {
+      it.todo('should update currently logged in user');
+    });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {
+      it.todo('should create a bookmark');
+    });
+
+    describe('Get all bookmarks', () => {
+      it.todo('should return all bookmarks');
+    });
+
+    describe('Get bookmark by id', () => {
+      it.todo('should return a bookmark by id');
+    });
+
+    describe('Edit bookmark', () => {
+      it.todo('should update a bookmark');
+    });
+
+    describe('Delete bookmark', () => {
+      it.todo('should delete a bookmark');
+    });
+  });
 });
